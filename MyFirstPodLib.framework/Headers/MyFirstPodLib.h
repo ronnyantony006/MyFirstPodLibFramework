3152fcfa736179666f0ed3f13ed12af64c7bfd0c
Ronny Antony <ronny.antony@zcocorporation.com>
2021-07-14 21:18:23 +0530

    Framework added.

---
+++ MyFirstPodLib.framework/Headers/MyFirstPodLib.h
@@ -0,0 +1,18 @@
+//
+//  MyFirstPodLib.h
+//  MyFirstPodLib
+//
+//  Created by Ronny Antony on 14/06/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyFirstPodLib.
+FOUNDATION_EXPORT double MyFirstPodLibVersionNumber;
+
+//! Project version string for MyFirstPodLib.
+FOUNDATION_EXPORT const unsigned char MyFirstPodLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFirstPodLib/PublicHeader.h>
+
+
